4ef165cdac223b15d5194b4544471fcb225b2770
Panashe Chipendo <106480889+panashechipendo@users.noreply.github.com>
2025-07-07 16:49:07 +0200

    Add repeated Prompting if user input incorrect

--- finance.py
+++ finance.py
@@ -6,6 +6,25 @@ options = ["Add Transaction", "View Balance", "Set Budget", "View Budget Status"
 def main():
     for i in range(len(options)):
         print(f"{i+1}. {options[i]}")
+
+    option = input("Select an Option: ")
+    while True:
+        try:
+            user_input = input("Choose a number from the options: ")
+            if options[int(user_input) - 1]:
+                if user_input == "1":
+                    add_transaction()
+                elif user_input == "2":
+                    load_data()
+        except IndexError:
+            print("No option at that number")
+            sys.exit(1)
+        except ValueError:
+            print("Not a valid input")
+
+
+
+    
     
 
 def add_transaction():
@@ -15,6 +34,15 @@ def load_data():
     ...
 
 def savedata():
+    ...
+
+def set_budget():
+    ...
+
+def calculate_budget_remaining():
+    ...
+
+def check_budget_alerts():
     ...
 
 
