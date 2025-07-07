b3b990e08661c271cc5815b3d04109429728b2e6
Panashe Chipendo <106480889+panashechipendo@users.noreply.github.com>
2025-07-07 15:39:13 +0200

    Display options

--- finance.py
+++ finance.py
@@ -1,1 +1,22 @@
 import csv
+import sys
+
+options = ["Add Transaction", "View Balance", "Set Budget", "View Budget Status", "Generate Report", "Export Data", "Exit"]
+
+def main():
+    for i in range(len(options)):
+        print(f"{i+1}. {options[i]}")
+    
+
+def add_transaction():
+    ...
+
+def load_data():
+    ...
+
+def savedata():
+    ...
+
+
+if __name__ == "__main__":
+    main()
